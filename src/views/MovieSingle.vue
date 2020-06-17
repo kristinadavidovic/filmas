ba2452520edfd4e8a6f55f927d9f816f95e78778
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2020-06-17 16:01:24 +0200

    🎨

--- src/views/MovieSingle.vue
+++ src/views/MovieSingle.vue
@@ -1,8 +1,27 @@
 <template>
-  <div v-if="movie">
-    <img :src="movie.backdrop_path" :alt="`${movie.title} poster image`" />
-    <h1>{{ movie.title }}</h1>
-    <img :src="movie.poster_path" :alt="`${movie.title} poster image`" />
+  <div class="content content--movie" v-if="movie">
+    <div class="movie-single">
+      <div class="movie-single__backdrop">
+        <div
+          class="movie-single__backdrop-image"
+          :style="`backgroundImage: url('${movie.backdrop_path}')`"
+        ></div>
+        <div class="movie-single__backdrop-fade"></div>
+      </div>
+      <div class="movie-single__content">
+        <div class="container movie-single__container">
+          <div class="movie-single__image-wrapper">
+            <img
+              :src="movie.poster_path"
+              :alt="`${movie.title} poster image`"
+            />
+          </div>
+          <div class="movie-single__info">
+            <h1>{{ movie.title }}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
